ab1cbe8b1f22da7a79b97b7f462aca7a6189ce7d
AnjaKa <Kaczmara@students.uni-marburg.de>
2014-05-15 10:24:50 +0200

    packages.R committed

--- packages.R
+++ packages.R
@@ -2,4 +2,17 @@ packages.used <- c("knitr","knitcitations"
                    ,"ggplot2"
                    ,"plyr"
                    ,"languageR"
+<<<<<<< HEAD
                    )
+=======
+                   ,"reshape2"
+                   ,"reshape"
+                   ,"sn"
+                   ,"car"
+                   )
+
+install.packages(packages.used,dependencies = TRUE)
+
+# Debian/Ubuntu-Nutzer  
+# sudo apt-get install libxml2-dev libcurl4-openssel-dev jags
+>>>>>>> 8af64a77af0d6c68d2fc6a98a146953044539800
