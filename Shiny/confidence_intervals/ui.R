0b8981e4381cd07bd118221a1d6aedac9d9b3496
AnjaKa <Kaczmara@students.uni-marburg.de>
2014-06-07 10:18:56 +0200

    commit 07.06.14.2

--- Shiny/confidence_intervals/ui.R
+++ Shiny/confidence_intervals/ui.R
@@ -1,4 +1,22 @@
+# Copyright 2014, Phillip Alday
+#
+# This file is part of Confidence Intervals.
+#
+# Confidence Intervals is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 library(shiny)
+require(markdown)
 
 shinyUI(fluidPage(
   titlePanel("Confidence Intervals"),
@@ -39,6 +57,7 @@ shinyUI(fluidPage(
                                 ,value = 0.1)
                  , actionButton("runagain",label = "Run Again")                
     )
+<<<<<<< HEAD
     ,mainPanel(h2("Sample means")
                ,plotOutput("sample.means")
                #,h2("Confidence Interval (via t.test)")
@@ -47,6 +66,12 @@ shinyUI(fluidPage(
                ,plotOutput("sample.distributions")
                ,h2("Population")
                ,plotOutput("population.distribution")
+=======
+    ,mainPanel(h2("Sample distributions")
+               ,plotOutput("sample.distributions",height=768)
+               ,includeMarkdown("confidence_intervals.md")
+
+>>>>>>> e2d60c36c91674f21010a69b256d3e299f229819
     )
   )
   
